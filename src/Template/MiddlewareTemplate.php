c1a71edbba66fb48b325e96529f6067c876c06c4
Ryuk <41i.6h0rb4n1@gmail.com>
2020-09-03 19:24:49 +0430

    Add cli for creating controllers and middlewares

---
+++ src/Template/MiddlewareTemplate.php
@@ -0,0 +1,21 @@
+
+
+namespace <?= $namespace ?>;
+
+/**
+ * Class <?= $className ?>
+
+ *
+ * @package <?= $namespace ?>
+
+*/
+class <?= $className ?>
+
+{
+    public function handle()
+    {
+        // your middleware codes
+    
+        return true;
+    }
+}
